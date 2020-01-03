1b3d716ffdac0498c13685620837987fc5769cf0
didiavigad <58212304+didiavigad@users.noreply.github.com>
2020-01-03 15:11:47 +0200

    Update Graph_Algo.java

--- algorithms/Graph_Algo.java
+++ algorithms/Graph_Algo.java
@@ -191,52 +191,95 @@ public class Graph_Algo implements graph_algorithms{
 
 
 
-	/**
-	 * Returns true if and only if (iff) there is a valid path from EVREY node to each
-	 * other node. NOTE: assume directional graph - a valid path (a-->b) does NOT imply a valid path (b-->a).
-	 * @return
-	 */
-	@Override
 	public double shortestPathDist(int src, int dest) {
-		// TODO Auto-generated method stub
-		return 0;
+		List<node_data> shortListDist  = new LinkedList<node_data>();
+		shortListDist = shortestPath(src, dest);
+		if (shortListDist.size()==0) return Double.POSITIVE_INFINITY;
+		double dist =0;
+		for (node_data node: shortListDist) {
+			dist= dist + node.getWeight();
+		}
+		return dist;
 	}
-	/**
-	 * returns the length of the shortest path between src to dest
-	 * @param src - start node
-	 * @param dest - end (target) node
-	 * @return
-	 */
+
 	@Override
 	public List<node_data> shortestPath(int src, int dest) {
 		// TODO Auto-generated method stub
-
-		return null;
-
+	
+		for (node_data node: this.gAlgo.getV()) {
+			node.setWeight(Double.POSITIVE_INFINITY); // set weight infinity
+			node.setTag(0); // set unvisitied
+		}
+		
+		
+		gAlgo.getNode(src).setWeight(0.);
+		gAlgo.getNode(src).setInfo(src +" ");
+		LinkedList<node_data> list_short = new LinkedList<node_data>() ;
+		Comparator_list comp = new Comparator_list();
+		//insert all nodes
+		for (node_data node: this.gAlgo.getV()) {
+			list_short.add(node);
+		}
+		
+		while (gAlgo.getNode(dest).getTag()==0 && list_short.size()!=0) {
+			list_short.sort(comp);
+			node_data first = list_short.get(0);
+			for (edge_data edge : gAlgo.getE(first.getKey())) {
+				node_data vEdge = gAlgo.getNode(edge.getDest());
+				if (vEdge.getTag()==0) {
+					double minEdge = first.getWeight()+edge.getWeight();
+					if (minEdge<vEdge.getWeight()) {
+						vEdge.setInfo(first.getInfo()+ " " + vEdge.getInfo());
+						vEdge.setWeight(minEdge);
+					}
+				}
+			}
+			first.setTag(1);
+			list_short.remove(0);
+		}
+		
+		// until here its algorithm
+		
+		String str = gAlgo.getNode(dest).getInfo();
+		String [] strArr = str.split(" ");
+		LinkedList<node_data> list = new LinkedList<node_data>();
+		for (int i = 0; i < strArr.length; i++) {
+			int key = Integer.parseInt(strArr[i]);
+			node_data node = gAlgo.getNode(key);
+			list.add(node);
+		}
+		return list;
+	
 	}
+	class Comparator_list implements Comparator<node_data>{
 
-	/**
-	 * returns the the shortest path between src to dest - as an ordered List of nodes:
-	 * src--> n1-->n2-->...dest
-	 * see: https://en.wikipedia.org/wiki/Shortest_path_problem
-	 * @param src - start node
-	 * @param dest - end (target) node
-	 * @return
-	 */
-
+		@Override
+		public int compare(node_data o1, node_data o2) {
+			return (int)(o1.getWeight()-o2.getWeight());
+		}
+		
+	}
 	@Override
 	public List<node_data> TSP(List<Integer> targets) {
-		// TODO Auto-generated method stub
-		return null;
+		List<node_data> TSP_list = new LinkedList<node_data>();
+		node_data src  = gAlgo.getNode(targets.get(0));
+		if (targets.size()==1)  {
+			TSP_list.add(src);
+			return TSP_list;
+		}
+		for (int i = 0; i < targets.size()-1; i++) {
+			
+			int dest = targets.get(i+1);
+			node_data node = gAlgo.getNode(dest);
+			if (TSP_list.contains(node)) i++;
+			
+			List<node_data> listPath = shortestPath(i, i+1);
+			
+			TSP_list.addAll(listPath);	
+		}
+		return TSP_list;
 	}
-	/**
-	 * computes a relatively short path which visit each node in the targets List.
-	 * Note: this is NOT the classical traveling salesman problem, 
-	 * as you can visit a node more than once, and there is no need to return to source node - 
-	 * just a simple path going over all nodes in the list. 
-	 * @param targets
-	 * @return
-	 */
+
 	@Override
 	public graph copy() {
 		// TODO Auto-generated method stub
